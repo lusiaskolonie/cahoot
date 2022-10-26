eaef550b6bb2b38387f9b4c53e122eaef8642654
Kamolpop <kamolpop@thinknet.co.th>
2022-10-26 13:30:58 +0700

    Updates

---
+++ precache-manifest.1f391b7c7c0bca36ba8303ca08ee4bbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c7ab343156ba2ff78d254de5a40c6e",
+    "url": "/cahoots/index.html"
+  },
+  {
+    "revision": "1c522b820e8d6862e328",
+    "url": "/cahoots/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "cc23c8a6a3dc1e587268",
+    "url": "/cahoots/static/css/main.aa7cdbea.chunk.css"
+  },
+  {
+    "revision": "1c522b820e8d6862e328",
+    "url": "/cahoots/static/js/2.322af2d1.chunk.js"
+  },
+  {
+    "revision": "ca5027641e99b6e8abb6c3ffd86346c0",
+    "url": "/cahoots/static/js/2.322af2d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc23c8a6a3dc1e587268",
+    "url": "/cahoots/static/js/main.2005ce54.chunk.js"
+  },
+  {
+    "revision": "3e8f9c616b6339a39a3b",
+    "url": "/cahoots/static/js/runtime-main.5480bec4.js"
+  }
+]);
